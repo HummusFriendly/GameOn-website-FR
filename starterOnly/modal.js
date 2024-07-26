88ae21267352b01545e208cd2904b264b2817bdd
Anthony Dupuis <votre.email@example.com>
2024-07-26 18:41:39 +0200

    first step

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -7,10 +7,63 @@ function editNav() {
   }
 }
 
+const regex = /^[A-Za-zÀ-ÖØ-öø-ÿ]{2,}$/
+const emailRegex = /^[A-Za-z]{1,}[A-Za-z0-9._%+-]+@[A-Za-z.-]+\.[A-Za-z]{2,}$/;
+
+
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
+const modalBtnClose = document.querySelector(".close");
+const submitButton = document.querySelector(".btn-submit");
 const formData = document.querySelectorAll(".formData");
+const content = document.querySelector(".content");
+
+const firstNameInput = document.getElementById("first")
+const firstNameError = document.querySelector(".firstNameError")
+
+const emailInput = document.getElementById("email")
+const emailError = document.querySelector(".emailNameError")
+
+const validElements = {
+  firstNameIsValid : false,
+  lastNameIsValid: false,
+  emailIsValid: false,
+  birthdayIsValid: false,
+  isParticipationValid: false
+}
+
+function manageDisableButton() {
+  // Quand ça sera fini remplacer some par every
+  submitButton.disabled = !Object.values(validElements).some(item => item)
+}
+
+
+firstNameInput.addEventListener("input", () => {
+  validateInput(firstNameInput, regex, firstNameError, 'firstNameIsValid' );
+});
+
+emailInput.addEventListener("input", () => {
+  validateInput(emailInput, emailRegex, emailError, 'emailIsValid' );
+});
+
+function validateInput(input, regex, errorElement, isValidParameter) {
+  console.log(validElements)
+
+  if (!regex.test(input.value)) {
+    errorElement.style.display = "block";
+    input.classList.add("field-error");
+    validElements[isValidParameter] = false
+    // return false;
+  } else {
+    errorElement.style.display = "none";
+    input.classList.remove("field-error");
+    validElements[isValidParameter] = true
+    // return true;
+  }
+  manageDisableButton()  
+}
+
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -19,5 +72,10 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
+modalBtnClose.addEventListener("click", function() {
+  modalbg.style.display = "none";
+});
 
-
+function validate() {
+  console.log(validElements)
+}
